de258e263c3c13f98f4d14a68a97b1c3bdb959a0
charlesthk <charles@vingtcinq.io>
2018-04-30 15:44:56 +0200

    update post_gen hook

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -12,7 +12,7 @@ def main():
         data['django_secret_key'] = generate_secret_key()
         data['db_name'] = "{{ cookiecutter.project_slug }}"
     with open('.env.json', 'w') as env_file:
-        json.dump(data, env_file)
+        json.dump(data, env_file, indent=4, sort_keys=True)
 
 
 if __name__ == "__main__":
