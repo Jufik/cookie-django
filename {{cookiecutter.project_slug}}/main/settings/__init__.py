82e543a030feca132cfe2fa1c33a57602890f566
charlesthk <charles@vingtcinq.io>
2018-05-10 16:42:09 +0200

    fix base_dir

--- {{cookiecutter.project_slug}}/main/settings/__init__.py
+++ {{cookiecutter.project_slug}}/main/settings/__init__.py
@@ -20,7 +20,7 @@ from main.settings.rest import *
 from main.settings.imagekit import *
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
-BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.dirname(os.path.abspath(__file__))))
 
 
 # Quick-start development settings - unsuitable for production
