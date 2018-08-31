aa8622e3f877ea5636f902777fca3f8005dbad0b
charlesthk <charles@vingtcinq.io>
2018-08-31 13:12:31 +0200

    add test file

--- hooks/post_gen_project.py
+++ hooks/post_gen_project.py
@@ -13,7 +13,12 @@ def main():
         data['db_name'] = "{{ cookiecutter.project_slug }}"
     with open('.env.json', 'w') as env_file:
         json.dump(data, env_file, indent=4, sort_keys=True)
-
+    with open(".env.test.json", "r+") as test_env_file:
+        data = json.loads(test_env_file.read())
+        data['django_secret_key'] = generate_secret_key()
+        data['db_name'] = "{{ cookiecutter.project_slug }}"
+        test_env_file.seek(0)
+        json.dump(data, test_env_file, indent=4, sort_keys=True)
 
 if __name__ == "__main__":
     main()
--- {{cookiecutter.project_slug}}/main/settings/db.py
+++ {{cookiecutter.project_slug}}/main/settings/db.py
@@ -2,7 +2,7 @@ from main.jsonenv import env
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'ENGINE': env.get('db_engine', ''),
         'NAME': env.get('db_name', ''),
         'USER': env.get('db_user', ''),
         'HOST': env.get('db_host', ''),
