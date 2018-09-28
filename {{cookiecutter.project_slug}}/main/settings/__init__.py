da9f92f9a1ddd89208ccadda6d02061c42e98c67
charlesthk <charles@vingtcinq.io>
2018-09-28 14:37:06 +0200

    add ckeditor settings

--- {{cookiecutter.project_slug}}/main/settings/__init__.py
+++ {{cookiecutter.project_slug}}/main/settings/__init__.py
@@ -16,6 +16,7 @@ from main.jsonenv import env
 from main.settings.db import *
 from main.settings.raven import *
 from main.settings.mail import *
+from main.settings.ckeditor import *
 from main.settings.user import *
 from main.settings.testing import *
 from main.settings.rest import *
